ad59ee35e4273673bd54c691921a34a373319f5f
박예찬 <83604180+raetic@users.noreply.github.com>
2022-04-03 00:40:20 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 136 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int arr[1025][1025];
+int sum[1025][1025];
+int sum2[1025][1025];
+int n, m;
+int main() {
+	ios_base::sync_with_stdio(false);
+
+	cin.tie(NULL);
+
+	cout.tie(NULL);
+	cin >> n >> m;
+	int x;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> x;
+			arr[i][j] = x;
+			sum[i][j] = sum[i][j - 1] + x;
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			sum2[i][j] = sum2[i - 1][j] + sum[i][j];
+		}
+	}
+
+	
+	int s = 0;
+	for (int i = 1; i <= m; i++) {
+		s = 0;
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;	
+		cout << sum2[x2][y2]-sum2[x1-1][y2]-sum2[x2][y1-1]+sum2[x1-1][y1-1] << '\n';
+	}
+
+}
