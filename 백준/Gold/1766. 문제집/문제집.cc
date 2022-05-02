3709f2abbf548a8b60b081a7dc2e104b221e4ac2
박예찬 <83604180+raetic@users.noreply.github.com>
2022-05-02 18:08:26 +0900

    [Gold II] Title: 문제집, Time: 32 ms, Memory: 4248 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.cc
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -2,12 +2,11 @@
 #include<queue>
 #include<vector>
 using namespace std;
-priority_queue<int, vector<int>, greater<int>> pq;
-priority_queue<int, vector<int>, greater<int>> d[32001];
-int c[32001];
+priority_queue<int, vector<int>, greater<int>> pq; //쉬운 문제부터 저장하기 위해서
+priority_queue<int, vector<int>, greater<int>> d[32001]; //쉬운 문제부터 저장하기 위해서
+int c[32001];//문제를 풀기위해 풀어야하는 선행 문제들의 갯수를 저장하는 변수
 int n, m;
-bool flag[32001];
-vector<int> printV;
+bool flag[32001]; //이미 푼 문제인지 확인
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> m;
@@ -21,19 +20,19 @@ int main() {
 		if (c[i] == 0) {
 			flag[i] = true;
 			pq.push(i);
-			
+
 		}
 	}
 	while (!pq.empty()) {
 		int x = pq.top();
-		printV.push_back(x);
+		cout << x << ' ';
 		pq.pop();
 		int k = d[x].size();
 		for (int i = 0; i < k; i++) {
 			if (!flag[d[x].top()]) {
 				c[d[x].top()]--;
 				if (c[d[x].top()] <= 0) {
-				
+
 					pq.push(d[x].top());
 					flag[d[x].top()] = true;
 				}
@@ -41,5 +40,4 @@ int main() {
 			d[x].pop();
 		}
 	}
-	for (int i = 0; i < n; i++)cout << printV[i]<<' ';
 }
