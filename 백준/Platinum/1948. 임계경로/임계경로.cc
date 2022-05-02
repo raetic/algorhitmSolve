58fa234ac0065308ed0a0a99ae1a60b99d80b4d4
박예찬 <83604180+raetic@users.noreply.github.com>
2022-05-02 20:57:23 +0900

    [Platinum V] Title: 임계경로, Time: 616 ms, Memory: 10372 KB -BaekjoonHub

--- 백준/Platinum/1948. 임계경로/임계경로.cc
+++ 백준/Platinum/1948. 임계경로/임계경로.cc
@@ -4,37 +4,35 @@
 #include<tuple>
 using namespace std;
 int n, m, s, e;
-vector<tuple<int, int, int>> v[100001];//길
-vector<tuple<int,int,int>> rev[100001];
+vector <pair<int,int>> v[100001];//길
+vector<tuple<int, int, int>> rev[100001];
 int d[100001];
 bool flag[100001];
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
-		int x, y,z;
-		cin >> x >> y>>z;
-		v[x].push_back({ y,z,i });
+		int x, y, z;
+		cin >> x >> y >> z;
+		v[x].push_back({y,z});
 		rev[y].push_back({ x,z,i });
-		
+
 	}
 	cin >> s >> e;
-	queue<pair<int,int>> q;
+	queue<pair<int, int>> q;
 	q.push({ s,0 });
 	while (!q.empty()) {
 		int x = q.front().first;
 		int y = q.front().second;
 		q.pop();
 		for (int i = 0; i < v[x].size(); i++) {
-			if(d[get<0>(v[x][i])] < y + get<1>(v[x][i])) {
-				flag[get<2>(v[x][i])] = true;
-				d[get<0>(v[x][i])] = y + get<1>(v[x][i]);
-				q.push({ get<0>(v[x][i]),get<1>(v[x][i]) + y });
+			if (d[v[x][i].first] < y + v[x][i].second) {
+				d[v[x][i].first] = y + v[x][i].second;
+				q.push({ v[x][i].first,v[x][i].second + y });
 			}
 		}
-	}	
-	cout << d[e]<<'\n';
-	for (int i = 0; i < m; i++)flag[i] = false;
+	}
+	cout << d[e] << '\n';
 	int counter = 0;
 	q.push({ e, d[e] });
 	while (!q.empty()) {
@@ -42,7 +40,7 @@ int main() {
 		int y = q.front().second;
 		q.pop();
 		for (int i = 0; i < rev[x].size(); i++) {
-			if (!flag[get<2>(rev[x][i])]&&d[get<0>(rev[x][i])] == y - get<1>(rev[x][i])) {
+			if (!flag[get<2>(rev[x][i])] && d[get<0>(rev[x][i])] == y - get<1>(rev[x][i])) {
 				flag[get<2>(rev[x][i])] = true;
 				counter++;
 				q.push({ get<0>(rev[x][i]) ,y - get<1>(rev[x][i]) });
