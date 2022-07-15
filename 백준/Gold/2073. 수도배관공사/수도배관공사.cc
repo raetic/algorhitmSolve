deeea521593cf4b0d0f83d8684a105374b0c620f
박예찬 <83604180+raetic@users.noreply.github.com>
2022-07-15 20:16:36 +0900

    [Gold IV] Title: 수도배관공사, Time: 52 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/2073. 수도배관공사/수도배관공사.cc
+++ 백준/Gold/2073. 수도배관공사/수도배관공사.cc
@@ -4,7 +4,7 @@
 using namespace std;
 int dp[100002][2]; //길이, 개수
 int d, p;
-pair<int,int> arr[351]; //first->길이 , second-> 용량
+pair<int, int> arr[351]; //first->길이 , second-> 용량
 int main() {
 	cin >> d >> p;
 	for (int i = 1; i <= p; i++) {
@@ -14,10 +14,9 @@ int main() {
 	}
 	for (int i = 1; i <= p; i++) {
 		int l = arr[i].first;
-		for (int j = 1; j <= d; j++) {
-			if (j - l < 0)continue;
+		for (int j = l; j <= d; j++) {
 			if (l == j) {
-             dp[j][1] = max(dp[j][1], arr[i].second);			
+				dp[j][1] = max(dp[j][1], arr[i].second);
 			}
 			if (dp[j - l][0] != 0) {
 				int k = min(dp[j - l][0], arr[i].second);
